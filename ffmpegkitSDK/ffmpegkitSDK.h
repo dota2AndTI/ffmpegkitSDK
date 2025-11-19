adf51aba3e891bef6e3db6cf7c01637cc7f07051
cao yulong <az@duodianmeihao.com>
2025-11-19 17:08:37 +0800

    Initial commit: First version of ffmpegkit

--- ffmpegkitSDK/ffmpegkitSDK.h
+++ ffmpegkitSDK/ffmpegkitSDK.h
@@ -7,10 +7,9 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for ffmpegkitSDK.
 FOUNDATION_EXPORT double ffmpegkitSDKVersionNumber;
 
-//! Project version string for ffmpegkitSDK.
+
 FOUNDATION_EXPORT const unsigned char ffmpegkitSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ffmpegkitSDK/PublicHeader.h>
+
